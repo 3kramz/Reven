83305fbabbe16751870365ff5b7b5095e10bb14b
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-05-07 17:47:29 +0600

    inventory details updated

--- src/Components/Pages/InventoryDetails/InventoryDetails.js
+++ src/Components/Pages/InventoryDetails/InventoryDetails.js
@@ -4,7 +4,8 @@ import useLoadData from "../../Shared/Hooks/useLoadData"
 
 const InventoryDetails = () => {
     const {id }=useParams()
-    const inventory = useLoadData(`https://guarded-ravine-66276.herokuapp.com/inventory/${id}`)
+    const url= `https://guarded-ravine-66276.herokuapp.com/inventory/${id}`
+    const inventory = useLoadData(url)
    
     const [data, setData]=useState({})
     useEffect(()=>setData(inventory),[inventory])
@@ -22,6 +23,7 @@ const InventoryDetails = () => {
         }
         const info ={...data, stock,sold}
         setData(info)
+        console.log(data)
        
     }
 
@@ -44,10 +46,28 @@ const InventoryDetails = () => {
        else{return}
         e.target.reset()
     }
+
+    const handleSaveBtn = e=>{
+
+        fetch(`http://localhost:5000/inventory/${id}`, {    
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log('success', data);
+            alert('users added successfully!!!');
+          
+        })
+      
+    }
     
 
     return (
-        <div className='mt-10 container mx-auto'>
+        <div className='my-10 container mx-auto'>
             <div className='h-25 w-[83.4px] border-[#F44617] border-2 '><p className='text-white fs-[14px] bg-[#F44617] py-[5px] px-[10px] '>{condition}</p>
             </div>
             <div className='border-t-[#F44617] border-t-4  sm:flex'>
@@ -57,10 +77,10 @@ const InventoryDetails = () => {
                 <div className='sm:w-3/5 mt-2 sm:mt-0 '> 
                     <div className=' flex justify-between bg-[#f7f7f7] border-b-[#D4D4D4] border-b-2  h-[60px] text-left text-2xl  font-bold ' >
                         <p className=' flex items-center pl-5  text-[#F44617] '>{name}</p>
-                        <button onClick={handleDalivered} className=' flex items-center px-8 bg-[#F44617]  border-b-[#F44617]  text-white' > Delivered</button>
+                        <button onClick={handleSaveBtn} className=' flex items-center px-8 bg-[#F44617]  border-b-[#F44617]  text-white' > Save</button>
                     </div>
                     <div className='text-left ml-5 mt-4 text-xl'>
-                        <p className='mb-6'>{disc}</p>
+                        <p className='mb-2'>{disc}</p>
                         <p className='font-bold'>Product ID:   <span className='text-slate-800 font-thin'>{_id}</span></p>
                         <p className='font-bold'>Price:   $<span className='text-slate-800 font-thin'>{price}</span></p>
                         <p className='font-bold'>Type:   <span className='text-slate-800 font-thin'>{type}</span></p>
@@ -73,11 +93,15 @@ const InventoryDetails = () => {
                 </div>
              </div>
              <div >
+                    <div className=''>
+                            <button onClick={handleDalivered} className='bg-[#F44617]  w-64  py-2 text-white text-xl rounded  m-4 '>Delivered</button>
+                    </div>
                     <form className='flex w-full  mx-auto border-2' onSubmit={handleSubmit}>
                         <input className="w-3/5 sm:w-4/5 pl-3" placeholder='Add Stock'  type="text" name="stock"  />
-                        <input className="w-2/5 sm:w-1/5 bg-[#F44617] px-4 py-2 text-white"  type="submit" value="Update stock" />
+                        <input className="w-2/5 sm:w-1/5 bg-[#F44617] px-4 py-2 cursor-pointer text-white"  type="submit" value="Update stock" />
                     </form>
              </div>
+          
         </div>
     );
 };
